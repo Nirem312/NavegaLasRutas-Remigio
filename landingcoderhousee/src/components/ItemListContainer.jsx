eb003eaf7baf3d5e0d6a66480646e79cf128d5dd
Nirem312 <95861480+Nirem312@users.noreply.github.com>
2023-12-19 17:42:07 -0300

    Delete landingcoderhousee/src/components/ItemListContainer.jsx

--- landingcoderhousee/src/components/ItemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-export const Item = ({NombreDelItem, Precio, Imagen}) => {
-  return (
-    <ul>
-      <li><img src={Imagen} alt="" /></li>
-      <li><p>Item: {NombreDelItem}</p></li>
-      <li>Precio: {Precio}</li>
-    </ul>
-  )
-}
