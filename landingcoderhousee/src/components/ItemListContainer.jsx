bbff675f3b923037a9be30d089651cb16ec491a4
Nirem312 <95861480+Nirem312@users.noreply.github.com>
2023-12-19 17:44:28 -0300

    Add files via upload

---
+++ landingcoderhousee/src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+export const Item = ({NombreDelItem, Precio, Imagen}) => {
+  return (
+    <ul>
+      <div className="container">
+        <li><img src={Imagen} alt="Imagen del producto"/></li>
+        <li><p>Prenda: {NombreDelItem}</p></li>
+        <li>Precio: {Precio}</li>
+      </div>
+    </ul>
+  )
+}
