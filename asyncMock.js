c6a6d49236cd3de2acdc3a71bf3c4d3d8a8ee8f1
Nicolas~ <nicolasremigiofing@gmail.com~>
2024-01-10 14:51:34 -0300

    Actualiza ItemListContainer para cargar datos desde asyncMock

---
+++ asyncMock.js
@@ -0,0 +1,28 @@
+let datos = [
+  {
+    Id: 1,
+    NombreDelItem: "Remera",
+    Precio: 600,
+    Imagen: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6628cd7a-dd6a-43d3-a2b1-a9c921a93f98/d4q0qtb-45c9906e-6ad2-48e7-a071-c1c6b0904f5c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2MjhjZDdhLWRkNmEtNDNkMy1hMmIxLWE5YzkyMWE5M2Y5OFwvZDRxMHF0Yi00NWM5OTA2ZS02YWQyLTQ4ZTctYTA3MS1jMWM2YjA5MDRmNWMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.cEOmX0ZF4vX61G3xg-FlXtgXJC-EtC-XfGaIXu8aWG0"
+  },
+  {
+    Id: 2,
+    NombreDelItem: "Pantalones jean",
+    Precio: 1100,
+    Imagen: "https://pngimg.com/d/jeans_PNG5745.png"
+  },
+  {
+    Id: 3,
+    NombreDelItem: "Gorro",
+    Precio: 400,
+    Imagen: "https://assets.stickpng.com/images/580b57fbd9996e24bc43bf0e.png"
+  }
+]
+
+export const getProducts = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(datos);
+    }, 1000)
+  })
+}
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,11 +1,23 @@
-export const Item = ({NombreDelItem, Precio, Imagen}) => {
+import { useEffect, useState } from "react"
+import { getProducts } from "../../asyncMock";
+import { Item } from "../components/Item"
+
+export const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts()
+      .then(res => {
+        setProducts(res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, [])
+
   return (
-    <ul>
-      <div className="container">
-        <li><img src={Imagen} alt="Imagen del producto"/></li>
-        <li><p>Prenda: {NombreDelItem}</p></li>
-        <li>Precio: {Precio}</li>
-      </div>
-    </ul>
+    <div>
+      {products.map(prod => <Item key={prod.Id} {...prod}/>)}
+    </div>
   )
 }
